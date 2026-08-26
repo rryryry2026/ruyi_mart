6d6c4f81d38c7e551bbb8932bb28713cff9f80cd
毛林昊 <1974295430@qq.com>
2026-08-26 14:43:17 +0800

    feat(common):同意返回Result/ResultCode与全局异常处理

---
+++ src/main/java/com/ruyi/ruyi_mart/RuyiMartApplication.java
@@ -0,0 +1,13 @@
+package com.ruyi.ruyi_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuyiMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuyiMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruyi/ruyi_mart/RuyiMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruyi.ruyi_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuyiMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
