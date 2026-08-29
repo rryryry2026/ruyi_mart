bf54656e6937ce97e04f1fd38a2fe38f3893dc6e
毛林昊 <1974295430@qq.com>
2026-08-29 10:35:49 +0800

    fix(rocketmq): 修复RocketMQTemplate未注册 - @Import强制加载自动配置 + rocketmq配置顶层化

--- src/main/java/com/ruyi/ruyi_mart/RuyiMartApplication.java
+++ src/main/java/com/ruyi/ruyi_mart/RuyiMartApplication.java
@@ -1,9 +1,12 @@
 package com.ruyi.ruyi_mart;
 
+import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(RocketMQAutoConfiguration.class)
 public class RuyiMartApplication {
 
     public static void main(String[] args) {
